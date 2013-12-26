9b5672239678cd012af6f536d14631c13b6bf5ea
longzuyuan <longzuyuan@126.com>
2013-12-26 09:43:05 +0800

    try to fixed in 51job

--- play/snake/js/jquery.snake.js
+++ play/snake/js/jquery.snake.js
@@ -1,3 +1,5 @@
+
+$(function(){
 var Snake = {
 		
 	$map : {}, $cherry : {}, $overlay : {}, seg : {}, wallseg : {}, cache : {},	
@@ -400,5 +402,27 @@ Level = [
 		{seg : 40, top : 0, left : 390}
 	]
 ];
+
+	// FF2/Mac Opacity Bug
+	($.browser.mozilla && parseFloat($.browser.version) < 1.9 && 
+	navigator.appVersion.indexOf('Mac') !== -1) && 
+	$('body').css('-moz-opacity',.999);
+	
+	// IE6 background css flickering bug
+	(!$.browser.msie) && (function(){
+		try{document.execCommand('BackgroundImageCache', false, true);}
+		catch(e){};
+	})();
+	
+	// setup the game
+	Snake.setup();
+			
+	// start the game
+	$("a#start-game").click(function(e){
+		e.preventDefault();
+		Snake.newGame(true);
+	});
+
+});
 
 /* end of file */

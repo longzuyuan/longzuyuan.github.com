b630459be1eca2791c1165813c54d03605cbd1cd
longzuyuan <longzuyuan@126.com>
2013-12-26 08:53:35 +0800

    fixed the snake

--- play/snake/js/jquery.snake.js
+++ play/snake/js/jquery.snake.js
@@ -401,27 +401,4 @@ Level = [
 	]
 ];
 
-$(function(){
-	// FF2/Mac Opacity Bug
-	($.browser.mozilla && parseFloat($.browser.version) < 1.9 && 
-	navigator.appVersion.indexOf('Mac') !== -1) && 
-	$('body').css('-moz-opacity',.999);
-	
-	// IE6 background css flickering bug
-	(!$.browser.msie) && (function(){
-		try{document.execCommand('BackgroundImageCache', false, true);}
-		catch(e){};
-	})();
-	
-	// setup the game
-	Snake.setup();
-			
-	// start the game
-	$("a#start-game").click(function(e){
-		e.preventDefault();
-		Snake.newGame(true);
-	});
-
-});
-
 /* end of file */

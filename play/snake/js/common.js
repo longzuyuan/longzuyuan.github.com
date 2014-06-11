3d2038f5a0790071940d9e89f0af69b861e3ecca
Zuyuan Long <longzuyuan@126.com>
2014-06-11 12:51:24 +0800

    snake bugs

--- play/snake/js/common.js
+++ play/snake/js/common.js
@@ -1,15 +1,15 @@
 
 $(function(){
 	// FF2/Mac Opacity Bug
-	($.browser.mozilla && parseFloat($.browser.version) < 1.9 && 
-	navigator.appVersion.indexOf('Mac') !== -1) && 
-	$('body').css('-moz-opacity',.999);
+	//($.browser.mozilla && parseFloat($.browser.version) < 1.9 && 
+	//navigator.appVersion.indexOf('Mac') !== -1) && 
+	//$('body').css('-moz-opacity',.999);
 	
 	// IE6 background css flickering bug
-	(!$.browser.msie) && (function(){
-		try{document.execCommand('BackgroundImageCache', false, true);}
-		catch(e){};
-	})();
+	//(!$.browser.msie) && (function(){
+	//	try{document.execCommand('BackgroundImageCache', false, true);}
+	//	catch(e){};
+	//})();
 	
 	// setup the game
 	Snake.setup();
